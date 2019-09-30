4aa43b853c42f8602c092f2b8a0f5862ac56a9da
JACGGE <cucurull123@gmail.com>
2019-09-30 19:47:26 +0100

    Operativo 1/2 Preferencias Sin IA Sin Internet Sin FX

--- app/src/main/java/com/example/tresenraya/MainActivity.java
+++ app/src/main/java/com/example/tresenraya/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     // ECHO Icono hacerlo y ponerlo
     // ECHO Landscape pantalla
-    // TOeDO Prefrencias
+    // TOooooDO Prefrencias
     // TODO Start-Up pantalla
     // TODO Inteligencia Artificial
     // TODO WIFI 2 Jugadores
